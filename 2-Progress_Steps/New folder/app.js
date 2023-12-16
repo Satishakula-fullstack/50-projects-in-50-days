a1de5bcc4d5079f2bc7f7bfb4c4762615a2f4668
Satishakula <satish.akula@colosseumgroup.com>
2023-12-16 17:19:09 +0530

    50 Projects in 50 days

---
+++ 2-Progress_Steps/New folder/app.js
@@ -0,0 +1,43 @@
+const progress = document.querySelector(".progress");
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+btnNext.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  updateLineCircles();
+});
+
+btnPrev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  updateLineCircles();
+});
+
+function updateLineCircles() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+  if (currentActive == 1) {
+    btnPrev.disabled = true;
+  } else if (currentActive == 5) {
+    btnNext.disabled = true;
+  } else {
+    btnPrev.disabled = false;
+    btnNext.disabled = false;
+  }
+}
---
+++ 2-Progress_Steps/New folder2/app.js
@@ -0,0 +1,43 @@
+const progress = document.querySelector(".progress");
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+btnNext.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  updateCircles();
+});
+
+btnPrev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  updateCircles();
+});
+
+function updateCircles() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+  if (currentActive == 4) {
+    btnNext.disabled = true;
+  } else if (currentActive == 1) {
+    btnPrev.disabled = true;
+  } else {
+    btnPrev.disabled = false;
+    btnNext.disabled = false;
+  }
+}
