a297f62ed253eb7d5fd282fc677a99cc94b721eb
Satish A <satishakula164@gmail.com>
2024-01-24 16:04:16 +0530

    adding styles and js

--- 10-sound-board/app.js
+++ 10-sound-board/app.js
@@ -6,7 +6,10 @@ sounds.forEach((sound) => {
   const btn = document.createElement("button");
   btn.classList.add("btn");
   btn.innerText = sound;
-  console.log(sound);
+
+  btn.addEventListener("click", () => {
+    document.getElementById(sound).play();
+  });
 
   document.getElementById("buttons").appendChild(btn);
 });
